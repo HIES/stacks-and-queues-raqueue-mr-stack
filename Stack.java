e988c71c23c71fc17ee48f14d67fecf5e6f96e74
Same Basket <30935970+sambaskin@users.noreply.github.com>
2017-11-29 09:42:55 -0500

    Add files via upload

--- Stack.java
+++ Stack.java
@@ -1,20 +1,81 @@
+import java.lang.Exception;
+import java.util.EmptyStackException;
+
 public class Stack<E>
 {
-	public Stack()
-	{}
+    private int top;
+    private Object[] stack;
 
-	public void push(E item)
-	{}
+    public Stack()
+    {
+        stack = new Object[3];
+        top = -1;    
 
-	public E pop()
-	{return null;}
+    }
 
-	public E peek()
-	{return null;}
+    public void push(E item)
+    {
+        if(top < stack.length-1)
+        {
+            top++;
+            stack[top] = item;
 
-	public boolean isEmpty()
-	{return false;}
+            
+        }
+        else
+        {   
+            
+             Object[] tempStack = new Object[stack.length];
+            Object[] stack = new Object[(tempStack.length)*2];
+            for(int i = 0; i < tempStack.length; i++)
+            {
+                stack[i] = tempStack[i];
+            }
+            
+            stack[top] = item;
+            
 
-	public int size()
-	{return -1;}
+        }
+    }
+
+    public E pop()
+    {
+        if(top == -1 || stack[top] == null)
+        {
+            throw new EmptyStackException();
+        }
+        else
+        {
+        E del = (E)stack[top];
+        stack[top] = null;
+        top--;
+        return del;
+        }
+
+    }
+
+    public E peek()
+    {
+        if(top == -1 || stack[top] == null)
+            throw new EmptyStackException();
+        
+        else
+            return (E)stack[top];
+
+    }
+
+    public boolean isEmpty()
+    {
+        if(top == -1 || stack[top] == null)
+            return true;
+        
+        else
+            return false;
+        
+    }
+
+    public int size()
+    {
+        return top+1;
+    }
 }
